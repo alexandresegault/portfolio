072b1561d9db42e94249333660a1d64e704d914e
alexandresegault <alexandresegault@gmail.com>
2021-02-17 21:48:09 +0100

    ajou des liens pour les projets

--- src/components/Board.js
+++ src/components/Board.js
@@ -45,6 +45,14 @@ class Board extends React.Component {
               Trouver des recettes de cocktails en fonction d'ingrédients
               choisis
             </li>
+            <li>
+              <a
+                href="https://gijix.github.io/CodeMeMore-project-1-Cocktail/"
+                target="blank"
+              >
+                Lien vers le Site
+              </a>
+            </li>
           </ul>
         </div>
         <div className="froggy" onClick={this.frogVisible}>
@@ -55,11 +63,19 @@ class Board extends React.Component {
             thumbstack="frogThumbstack"
           />
           <ul className={this.state.frog ? "visible" : "invisible"}>
-            <li>Projet de groupe</li>
+            <li>Projet de groupe toujours en cours</li>
             <li>Réalisé en HTML, CSS, JavaScript et React.js</li>
             <li>Consommation de 2 REST API</li>
             <li>Objectif ? </li>
             <li>Trouver des activités touristiques en fonction de la météo</li>
+            <li>
+              <a
+                href="https://github.com/WildCodeSchool/remote-js-202011-froggy-devs"
+                target="blank"
+              >
+                Lien vers le Github
+              </a>
+            </li>
           </ul>
         </div>
         <div className="war" onClick={this.warVisible}>
@@ -74,6 +90,14 @@ class Board extends React.Component {
             <li>Réalisé en HTML, CSS et JavaScript</li>
             <li>Objectif ? </li>
             <li>Jouer à la bataille contre l'ordinateur</li>
+            <li>
+              <a
+                href="https://alexandresegault.github.io/warGame/"
+                target="blank"
+              >
+                Envie de jouer ?
+              </a>
+            </li>
           </ul>
         </div>
         <div className="calculator" onClick={this.calcVisible}>
@@ -88,6 +112,14 @@ class Board extends React.Component {
             <li>Réalisé en HTML, CSS, JavaScript et React.js</li>
             <li>Objectif ? </li>
             <li>Reproduire une mini calculette fonctionnelle</li>
+            <li>
+              <a
+                href="https://alexandresegault.github.io/calculatrice/"
+                target="blank"
+              >
+                Besoin de calculer ?
+              </a>
+            </li>
           </ul>
         </div>
       </div>
